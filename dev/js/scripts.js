55a6c8febda5d33f935062952f7942174ddeb359
serafla <63385649+serafla@users.noreply.github.com>
2020-05-14 13:47:09 -0400

    added link styles / fixed footer and header

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,22 @@
 
+var $burger = $("#burger-icon");
+var $nav = $('nav[data-nav]');
+var $navA = $('nav[data-nav] a');
+var isVisible = false;
+
+function mouseClick(){
+
+    console.log("click");
+    if(isVisible === false){
+        $nav.show();
+        isVisible = true;
+    }
+    else{
+        $nav.hide();
+        isVisible = false;
+    }
+}
+
+$burger.on( "click", mouseClick);
+
+$navA.on( "click", mouseClick);
